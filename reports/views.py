0b5a884e03c854bb8b765d868b13ee488e835317
James Vanstone <jvanstone.work@gmail.com>
2024-12-09 11:27:26 +0000

    add basic template for displaying missions on home page

--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Mission
 
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse("<h1> Hello World!</h1>")
+#def home_page_view(request):
+    #return HttpResponse("<h1> Hello World!</h1>")
+
+class HomePage(generic.ListView):
+    queryset = Mission.objects.all()
+    template_name = "home_page.html"
