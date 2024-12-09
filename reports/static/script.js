5bcfa30be910e1757f3b8cb91bd93af5c0b9ce5c
James Vanstone <jvanstone.work@gmail.com>
2024-12-09 17:23:11 +0000

    add hero image, favicon, logo, and style home page.

--- reports/static/script.js
+++ reports/static/script.js
@@ -1,7 +1,7 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 document.addEventListener("DOMContentLoaded", (event) => {
     var menuToggle = document.querySelector(".burger");
-    var menu = document.querySelector('nav ul');
+    var menu = document.querySelector('.mobile-nav');
 
     menuToggle.addEventListener('click', () => {
         let isVisible = menu.style.display === 'grid';
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     // Ensure the menu resets correctly on window resize
     window.addEventListener('resize', () => {
-        if (window.innerWidth >= 640) {
+        if (window.innerWidth >= 700) {
             menu.style.display = 'block';
         } else {
             menu.style.display = 'none';
