9433db56c14cb2a963de375e0857e0269a4d6152
James Vanstone <jvanstone.work@gmail.com>
2024-12-11 11:03:26 +0000

    add crud functionality to my_reports_page

--- reports/views.py
+++ reports/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from .models import Mission
+from django.contrib import messages
 
 # Create your views here.
 #def home_page_view(request):
@@ -21,4 +22,49 @@ class MyReportsPage(generic.ListView):
     model = Mission
     template_name = "reports/my_reports_page.html"
     def get_queryset(self):
-        return Mission.objects.filter(author=self.request.user)
+        return Mission.objects.filter(author=self.request.user).order_by('-date')
+
+class EditReport(generic.UpdateView):
+    model = Mission
+    fields = [
+        "date",
+        "pay",
+        "summary",
+        "location",
+        "type",
+        "participants",
+    ]
+    template_name_suffix = "_update"
+    success_url = "/my_reports"
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Mission updated successfully.')
+        return super().form_valid(form)
+
+class CreateReport(generic.CreateView):
+    model = Mission
+    fields = [
+        "date",
+        "pay",
+        "summary",
+        "location",
+        "type",
+        "participants",
+    ]
+    template_name_suffix = "_create"
+    success_url = "/my_reports"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, 'Mission created successfully.')
+        return super().form_valid(form)
+
+class DeleteReport(generic.DeleteView):
+    model = Mission
+    template_name_suffix = "_delete"
+    success_url = "/my_reports"
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Mission deleted successfully.')
+        return super().form_valid(form)
+
